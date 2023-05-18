73475a40fc9f4743d01e4cc0dd5578dc34b4ac17
IgorevichVladislav <reptail911@gmail.com>
2023-05-18 15:29:17 +0300

    portfolio

---
+++ js/main.js
@@ -0,0 +1,14 @@
+$(function() {
+
+    var mixer = mixitup('.work__list', {
+        animation: {
+            enable: true,
+            effects: 'scale fade',
+            duration: 700,
+            easing: 'cubic-bezier(0.645, 0.045, 0.355, 1)',
+            
+           
+        }
+    });
+
+});
